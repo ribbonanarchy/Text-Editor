60c6c07787d1a14ed0eb5c575fcbc3e9e2484fd9
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-16 21:30:55 -0500

    package.json updates and webpack plugins added to config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+       // Webpack plugin that generates our html file and injects our bundles. 
+       new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor Webpack Plugin'
+      }),
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'Text-Editor',
+        description: 'A place to edit your text.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
